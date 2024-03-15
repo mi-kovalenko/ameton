a5f9988dd1ed2f4c609adc259d944ca8371146ca
Maria <mi.kovalenko@mail.ru>
2024-03-15 18:54:30 +0400

    push

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,25 @@
 #!/usr/bin/env sh
 
-# abort on errors
+# остановить публикацию при ошибках
 set -e
 
-# build
+# сборка
 npm run build
 
-# navigate into the build output directory
+# переход в каталог с результатами сборки
 cd dist
 
-# если вы деплоите на кастомный домен
+# если вы деплоите на кастомный домен, раскомментируйте следующую строку и измените её
 # echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
+# если вы деплоите на https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+
+# если вы деплоите на https://<USERNAME>.github.io/<REPO>, замените <USERNAME> и <REPO> на ваши
 git push -f git@github.com:mi-kovalenko/ameton.git master:gh-pages
 
 cd -
